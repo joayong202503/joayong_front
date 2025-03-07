5b351763af694248b8db7099c81d4eb20a06a15c
mjkim41 <mj10283@naver.com>
2025-03-07 15:25:33 +0900

    [chore/initial-setup/mj] Initial project setup with folder structure and dependency configuration

---
+++ src/pages/TestPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TestPage = () => {
+    return (
+        <div>
+            <h1>초기 세팅 단계에서 테스트를 위한 페이지입니다.</h1>
+        </div>
+    );
+};
+
+export default TestPage;
