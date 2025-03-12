2ac6ee36368f16950256450fe9b9041f85cf962c
mjkim41 <mj10283@naver.com>
2025-03-12 17:26:13 +0900

    Revert "initial commit"
    This reverts commit 353f134f3bd38179910d5b3eb4685b70e5ece25c.

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,35 @@
-// Redux Provider + AppContent를 제공하는 파일
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-import './styles/global.module.scss';
-import AppContent from "./AppContent.jsx";
-import {Provider} from "react-redux";
+function App() {
+  const [count, setCount] = useState(0)
 
-const App = () => {
-
-    return (
-        <>
-            {/*<Provider store={store}>*/}
-                <AppContent />
-            {/*</Provider>*/}
-        </>
-    );
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
 export default App
