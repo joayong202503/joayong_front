712a1b309b5a9a5a68108c0edbfb73b33dacbd14
chchch928 <kbanda@naver.com>
2025-03-13 01:23:02 +0900

    [feature/routerSetting] Initial router setup complete

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -3,12 +3,105 @@ import TestFortTalentCategoryPage from "../store/testPages/TestFortTalentCategor
 import TestForRegionCategoryPage from "../store/testPages/TestForRegionCategoryPage.jsx";
 
 const router = createBrowserRouter([
+    // 로그인 관련 경로
+    {
+        path: '/signup',
+        element: <AuthLayout isLoginPage ={false}/>,
+        children: [
+            {
+                index: true,
+                element: <SignupPage />
+            }
+        ]
+    },
+    {
+      path: '/login',
+      element: <AuthLayout isLoginPage={true}/>,
+      children: [
+          {
+              index: true,
+              element: <LoginPage/>
+          }
+      ]
+
+    },
+    // 메인페이지
     {
         path: '/',
-        element: <TestForRegionCategoryPage />,
-        // errorElement:
-        // children:
+        element: <MainPage />,
+    },
+
+    {
+        path:'/exchanges',
+        element: <ExchangesLayout/>,
+        children: [
+            {
+                index: true,
+                // 전체 게시글
+                element: <ExchangeListPage/>
+            },
+            {
+               path: 'new',
+               element: <AuthRequired>
+                            <ExchangeCreatePage/>
+                        </AuthRequired>
+            },
+            {
+                path: ':exchangeId',
+                element: <ExchangeDetailPage/>
+            },
+            {
+                path:':exchangeId/edit',
+                element: <AuthRequired>
+                            <ExchangeEditPage />
+                         </AuthRequired>
+            },
+            {
+                path: ':exchangeId/request',
+                element: <AuthRequired>
+                            <ExchangeRequestPage />
+                         </AuthRequired>
+            }
+        ]
+    },
+    // 매칭관리
+    {
+        path: '/matches',
+        element: <AuthRequired>
+                    <MatchesPage/>
+                 </AuthRequired>
+    },
+    {
+        path: '/chat',
+        element: <ChatLayout/>,
+        children: [
+            {
+                path: ':matchId',
+                element: <AuthRequired>
+                            <ChatPage/>
+                         </AuthRequired>
+            }
+        ]
+    },
+    // 프로필 페이지
+    {
+        path: '/profile/:username',
+        element: <ProfilePage/>
+    },
+    {
+        path: '/settings/profile',
+        element: <AuthRequired><ProfileSettingPage /></AuthRequired>
+    },
+    {
+        path: '/about',
+        element: <AboutPage />
+    },
+    // 잘못된 경로로 접근 시 홈으로 리다이렉트
+    {
+        path: '*',
+        element: <Navigate to="/" replace/>
     }
+
 ]);
 
 
