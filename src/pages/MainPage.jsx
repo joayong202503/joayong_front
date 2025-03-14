fd78d143384c0ef48c4a7f27eb4acd8d6b251b61
chchch928 <kbanda@naver.com>
2025-03-14 15:02:03 +0900

    [feature/mainPage] mainPage initial completion

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -3,29 +3,33 @@ import styles from './MainPage.module.scss'
 import Card from "../components/common/Card.jsx";
 import { GoChevronLeft } from "react-icons/go";
 import { GoChevronRight } from "react-icons/go";
+import mainPhoto from "../assets/images/mainPage.jpeg"
+
 
 
 const MainPage = () => {
+
   return (
     <>
       <div className={styles.mainContainer}>
         <section className={styles.topContainer}>
-          {/*<Link to ="/about">*/}
-          <div className={styles.titleContainer}>
-            <h1>재능을 교환하고, 같이 성장합니다</h1>
+          <div className={styles.photoContainer}>
+            <img className={styles.mainImage} src={mainPhoto}/>
+            <div className={styles.contentContainer}>
+              <h1>재능을 교환하고, 같이 성장합니다.</h1>
+              <p> 전문지식과 기술을 나누고, 교환하며 더 나은 사회를<br/>
+                함께 만들어가는 재능교환 플랫폼 입니다.
+              </p>
+            </div>
           </div>
-          <div className={styles.contentContainer}>
-            <p> 전문지식과 기술을 나누고, 교환하며 더 나은 사회를<br/>
-              함께 만들어가는 재능교환 플랫폼 입니다.
-            </p>
-          </div>
-          {/*</Link>*/}
+
         </section>
         <section className={styles.bottomContainer}>
           <div className={styles.titleContainer}>
             <h2> 최근 등록 스킬</h2>
           </div>
           <div className={styles.cardContainer}>
+            {/*아직 임시로 만들어놓은 화살표 시간 가능하면 구현*/}
             <button className={styles.arrows}><GoChevronLeft /></button>
             <Card/>
             <Card/>
