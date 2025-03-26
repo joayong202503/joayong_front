3b58b11ef4a3c94e1a6cfb4c8350194b643d63e5
samchi <seyoung0314@gmail.com>
2025-03-26 18:36:04 +0900

    [feature/login] add logo to login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,22 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Eye, EyeOff } from 'lucide-react';
-import styles from './LoginPage.module.scss';
-import { useDispatch, useSelector } from 'react-redux';
-import { login } from '../store/slices/authSlice';
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { Eye, EyeOff } from "lucide-react";
+import styles from "./LoginPage.module.scss";
+import { useDispatch, useSelector } from "react-redux";
+import { login } from "../store/slices/authSlice";
+import logoImage from "../assets/images/logo-big.png";
 
 const LoginPage = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
   const [rememberMe, setRememberMe] = useState(false);
-  const [emailError, setEmailError] = useState('');
+  const [emailError, setEmailError] = useState("");
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const { status, error } = useSelector((state) => state.auth); // ✅ 로그인 상태 가져오기
 
   useEffect(() => {
-    const savedEmail = localStorage.getItem('rememberedEmail');
+    const savedEmail = localStorage.getItem("rememberedEmail");
     if (savedEmail) {
       setEmail(savedEmail);
       setRememberMe(true);
@@ -26,13 +27,13 @@ const LoginPage = () => {
   const validateEmail = (email) => {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!email) {
-      setEmailError('이메일을 입력해주세요.');
+      setEmailError("이메일을 입력해주세요.");
       return false;
     } else if (!emailRegex.test(email)) {
-      setEmailError('유효한 이메일 주소를 입력해주세요.');
+      setEmailError("유효한 이메일 주소를 입력해주세요.");
       return false;
     }
-    setEmailError('');
+    setEmailError("");
     return true;
   };
 
@@ -44,11 +45,11 @@ const LoginPage = () => {
       .unwrap() // Thunk 결과를 기다림
       .then(() => {
         if (rememberMe) {
-          localStorage.setItem('rememberedEmail', email);
+          localStorage.setItem("rememberedEmail", email);
         } else {
-          localStorage.removeItem('rememberedEmail');
+          localStorage.removeItem("rememberedEmail");
         }
-        navigate('/'); // 로그인 성공 후 홈으로 이동
+        navigate("/"); // 로그인 성공 후 홈으로 이동
       })
       .catch((err) => {
         console.error(err); // 오류 처리
@@ -57,6 +58,9 @@ const LoginPage = () => {
 
   return (
     <div className={styles.container}>
+      <div className={styles.logoContainer}>
+        <img src={logoImage} alt="logo사진" onClick={() => navigate('/')} />
+      </div>
       <h2 className={styles.title}>로그인</h2>
       <form onSubmit={handleLogin} className={styles.form}>
         <div>
@@ -69,28 +73,28 @@ const LoginPage = () => {
               setEmail(e.target.value);
               if (emailError) validateEmail(e.target.value);
             }}
-            className={styles['input-box']}
+            className={styles["input-box"]}
             placeholder="이메일 주소를 입력하세요"
             required
           />
-          {emailError && <p className={styles['error-text']}>{emailError}</p>}
+          {emailError && <p className={styles["error-text"]}>{emailError}</p>}
         </div>
 
         <div>
           <label htmlFor="password">비밀번호</label>
-          <div className={styles['password-container']}>
+          <div className={styles["password-container"]}>
             <input
               id="password"
-              type={showPassword ? 'text' : 'password'}
+              type={showPassword ? "text" : "password"}
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              className={styles['input-box']}
+              className={styles["input-box"]}
               placeholder="비밀번호를 입력하세요"
               required
             />
             <button
               type="button"
-              className={styles['toggle-icon']}
+              className={styles["toggle-icon"]}
               onClick={() => setShowPassword(!showPassword)}
             >
               {showPassword ? <EyeOff size={18} /> : <Eye size={18} />}
@@ -107,18 +111,22 @@ const LoginPage = () => {
           />
           <label htmlFor="remember">로그인 정보 기억하기</label>
         </div>
-        <div className={styles['text-center']}>
+        <div className={styles["text-center"]}>
           <p>계정이 없으신가요?</p>
-          <p onClick={() => navigate('/signup')} className={styles.link}>
+          <p onClick={() => navigate("/signup")} className={styles.link}>
             회원가입
           </p>
         </div>
 
-        <button type="submit" disabled={status === 'loading'} className={styles['btn-primary']}>
-          {status === 'loading' ? '로그인 중...' : '로그인'}
+        <button
+          type="submit"
+          disabled={status === "loading"}
+          className={styles["btn-primary"]}
+        >
+          {status === "loading" ? "로그인 중..." : "로그인"}
         </button>
 
-        {error && <p className={styles['error-text']}>{error}</p>}
+        {error && <p className={styles["error-text"]}>{error}</p>}
       </form>
     </div>
   );
