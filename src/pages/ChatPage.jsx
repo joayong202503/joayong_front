b46ed925d346d9e6bbe20171b96f08f48872f9ec
samchi <seyoung0314@gmail.com>
2025-03-28 19:25:47 +0900

    [feature/chat] added post information and hyperlink for the chat room

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -2,8 +2,8 @@ import React, { useEffect, useReducer, useState } from "react";
 import RtcRoom from "../components/ChatPage/RtcRoom";
 import { Provider, useSelector } from "react-redux";
 import { store } from "../store/index.js";
-import { useParams,useLocation } from "react-router-dom";
-import { fetchRtcRoomId } from "../services/rtcApi.js";
+import { useParams,useLocation,useNavigate } from "react-router-dom";
+import { fetchRtcRoomId,fetchPostInfo } from "../services/rtcApi.js";
 import styles from './ChatPage.module.scss';
 import ChatRoom from "../components/chat/ChatRoom.jsx";
 
@@ -13,9 +13,12 @@ const ChatPage = () => {
   const [roomId, setRoomId] = useState(null);
   const [isNew, setIsNew] = useState(null);
   const [error, setError] = useState(null);
+  const [postData, setPostData] = useState(null);
   const location = useLocation();
   const user1 = location.state?.name1 || "알 수 없음";
   const user2 = location.state?.name2 || "알 수 없음";
+
+  const navigate = useNavigate();
 
   console.log("messageId : ", messageId);
 
@@ -33,6 +36,22 @@ const ChatPage = () => {
     };
 
     fetchData();  
+  }, []); 
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await fetchPostInfo(messageId);
+        console.log("data2 : ", data);
+        setPostData(data);
+
+      } catch (error) {
+        console.error("Error fetching RTC room ID:", error);
+        setError(error); // 에러 상태 처리
+      }
+    };
+
+    fetchData();  
   }, [messageId]); 
 
   // rtc 서버에 접근하는 이름은 abc@aaa.com 일때 abcaaa로 (영어/숫자로만 가능해서)
@@ -40,13 +59,26 @@ const ChatPage = () => {
   const username = parts[0] + parts[1].split(".")[0];
 
     // roomId가 로딩 중이면 로딩 화면을 표시
-    if (roomId === null && isNew === null) {
+    if (roomId === null && isNew === null && postData === null) {
       return <div>Loading...</div>;  // roomId가 없을 때 로딩 표시
     }
+
+    const handleClick = () => {
+      if (postData && postData.postId) {
+        navigate(`/exchanges/${postData.postId}`);
+      }
+    };
 
   return (
     <>
       <Provider store={store}>
+      {postData && (
+          <div>
+            <span onClick={handleClick} style={{ cursor: "pointer", color: "blue" }}>
+              {postData.writer} ({postData.talentTName}) &lt;-&gt; {postData.sender} ({postData.talentGName})
+            </span>
+          </div>
+        )}
         <div className={styles.wrap}>
           <div className={styles.videoContainer}>
             <RtcRoom roomCode={roomId} username={username} isNew={isNew} />
--- src/services/rtcApi.js
+++ src/services/rtcApi.js
@@ -26,6 +26,25 @@ export const fetchRtcRoomId = async (messageId) => {
   }
 };
 
+export const fetchPostInfo = async (messageId) => {
+  try {
+    // fetchWithAuth 함수를 사용하여 API 호출
+    const response = await fetchWithAuth(
+      `${API_URL}${AUTH_REQUIRED}/message/postInfo/${messageId}`
+    );
+
+    if (!response.ok) {
+      throw new Error("API 호출 실패: " + response.status);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("사용자 프로필을 가져오는데 실패했습니다:", error);
+    throw error;
+  }
+};
+
 export const fetchExitRtcRoomId = async (roomId) => {
   try {
     // fetchWithAuth 함수를 사용하여 API 호출
